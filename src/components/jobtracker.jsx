f340d6426371313270b6f35a13d613f0295f7b62
nisi_pop_desktop <49096264+niallsiow@users.noreply.github.com>
2025-06-09 23:53:40 +0100

    Add basic form for submitting new job application

--- src/components/jobtracker.jsx
+++ src/components/jobtracker.jsx
@@ -1,7 +1,24 @@
 import { useEffect, useState } from "react";
 
+
+function Job({ jobInfo }){
+    if (jobInfo) {
+        return (
+            <>
+            <h2>Job Title: { jobInfo.title }</h2>
+            <h2>Company: { jobInfo.company }</h2>
+            <h2>Source: { jobInfo.source }</h2>
+            <h2>Stage: {jobInfo.stage }</h2>
+        </>
+        )
+    }
+    else {
+        return (<></>)
+    }
+}
+
 function JobTracker(){
-    const [title, setTitle] = useState("");
+    const [jobInfoList, setJobInfoList] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:8080')
@@ -9,14 +26,24 @@ function JobTracker(){
                 return response.json();
             })
             .then((response) => {
-                setTitle(response.title);
+                console.log(response);
+                console.log(response[0]);
+                setJobInfoList(response);
             })
-    }), [];
+    }, []);
 
     return (
         <>
             <h1>Job Tracker</h1>
-            <h2>{ title }</h2>
+            { jobInfoList.map((jobInfo) => {
+             return <Job jobInfo={jobInfo} />
+            })}
+
+            <form action="http://localhost:8080" method="post">
+                <label htmlFor="title">Job Title</label>
+                <input name="title" id="title"/>
+                <button type="submit">Save</button>
+            </form>
         </>
     );
 }
